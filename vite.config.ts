bfa8d7a254116c8052ef061bee240499a742cdc4
Pedro Serapio <pedro.mesopa@gmail.com>
2023-10-08 14:26:14 +0800

    fix: build

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/entherapy.com/',
   plugins: [react(), svgr()],
   build: {
     outDir: 'docs',
